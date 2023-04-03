992905d3bf142654532651d8882d77d4b851be7f
derya <derya@boonandmoil.dev>
2023-04-03 13:04:00 +0100

    Resolves #2

--- RollTheDice/ContentView.swift
+++ RollTheDice/ContentView.swift
@@ -8,93 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var diceResult: Int? = nil
-    @State private var previousRolls = [Int]()
+    @State private var diceResult: [Int] = []
+    @State private var previousRolls: [[Int]] = []
     @State private var numberOfDice = 1
     @State private var sidesOfDice = 6
     
     let diceOptions = [4, 6, 8, 10, 12, 20, 100]
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Dice Roller")
-                .font(.largeTitle)
-                .bold()
-                .accessibilityLabel("Dice Roller")
-            
-            HStack {
-                VStack {
-                    Text("Number of Dice: ")
-                    Picker("Number of Dice", selection: $numberOfDice) {
-                        ForEach(1...10, id: \.self) { number in
-                            Text("\(number)")
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 120, height: 100)
-                    .clipped()
-                }
-                .accessibilityLabel("Number of dice picker")
-                
-                VStack {
-                    Text("Sides of Dice:")
-                    Picker("Sides of Dice", selection: $sidesOfDice) {
-                        ForEach(diceOptions, id: \.self){ sides in
-                            Text("\(sides)")
-                        }
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(width: 120, height: 100)
-                    .clipped()
-                }
-                .accessibilityLabel("Sides of dice picker")
-            }
-            
-            Text(diceResult != nil ? "Current Roll: \(diceResult!)" : "Roll the dice to start")
-                .font(.title)
-                .bold()
-                .accessibilityLabel("Current Roll")
-                .accessibilityValue(diceResult != nil ? "\(diceResult!)" : "No roll yet")
-            
-            Button(action: rollDice) {
-                Text("Roll Dice")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                
-            }
-            .accessibilityLabel("Roll dice")
-            
-            VStack(alignment: .leading) {
-                Text("Previous Rolls:")
-                    .font(.title2)
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("Dice Roller")
+                    .font(.largeTitle)
                     .bold()
-                    .accessibilityLabel("Previous Rolls")
+                    .accessibilityLabel("Dice Roller")
                 
-                ScrollView {
-                    ForEach(previousRolls, id: \.self) { roll in
-                        Text("Roll: \(roll)")
+                HStack {
+                    VStack {
+                        Text("Number of Dice: ")
+                        Picker("Number of Dice", selection: $numberOfDice) {
+                            ForEach(1...10, id: \.self) { number in
+                                Text("\(number)")
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(width: 120, height: 100)
+                        .clipped()
+                    }
+                    .accessibilityLabel("Number of dice picker")
+                    
+                    VStack {
+                        Text("Sides of Dice:")
+                        Picker("Sides of Dice", selection: $sidesOfDice) {
+                            ForEach(diceOptions, id: \.self){ sides in
+                                Text("\(sides)")
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(width: 120, height: 100)
+                        .clipped()
+                    }
+                    .accessibilityLabel("Sides of dice picker")
+                }
+                
+                Text(diceResult.isEmpty ? "No rolls yet" : "Current Roll: \(formatDiceResults(diceResult))")
+                    .font(.title)
+                    .bold()
+                    .accessibilityLabel("Current Roll")
+                    .accessibilityValue(diceResult.isEmpty ? "No roll yet" : formatDiceResults(diceResult))
+                
+                Button(action: rollDice) {
+                    Text("Roll Dice")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    
+                }
+                .accessibilityLabel("Roll dice")
+                
+                VStack(alignment: .leading) {
+                    Text("Previous Rolls:")
+                        .font(.title2)
+                        .bold()
+                        .accessibilityLabel("Previous Rolls")
+                    
+                    if previousRolls.isEmpty {
+                        Text("No previous rolls")
                             .font(.body)
-                            .accessibilityLabel("Roll")
-                            .accessibilityValue("\(roll)")
+                            .italic()
+                    } else {
+                        ScrollView {
+                            ForEach(previousRolls, id: \.self) { roll in
+                                Text("Roll: \(formatDiceResults(roll))")
+                                    .font(.body)
+                                    .accessibilityLabel("Roll")
+                                    .accessibilityValue(formatDiceResults(roll))
+                            }
+                        }
                     }
                 }
+                .padding(.top)
+                
+                
             }
-            .padding(.top)
-            
-            
+            .padding()
         }
-        .padding()
     }
     
     func rollDice() {
-        let newRoll = Int.random(in: 1...6)
-        if let previousResult = diceResult {
-            previousRolls.append(previousResult)
+        var newRoll: [Int] = []
+        
+        for _ in 0..<numberOfDice {
+            newRoll.append(Int.random(in: 1...sidesOfDice))
         }
+        
+        if !diceResult.isEmpty {
+            previousRolls.append(diceResult)
+        }
+        
         diceResult = newRoll
+    }
+    
+    func formatDiceResults(_ results: [Int]) -> String {
+        results.map { String($0) }.joined(separator: ", ")
     }
 }
 
