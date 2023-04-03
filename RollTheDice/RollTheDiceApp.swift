4ff5d8480cefda700d5ce2505977f60b64992d1e
derya <derya@boonandmoil.dev>
2023-04-03 11:15:05 +0100

    Initial Commit

---
+++ RollTheDice/RollTheDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollTheDiceApp.swift
+//  RollTheDice
+//
+//  Created by Derya Antonelli on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RollTheDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
